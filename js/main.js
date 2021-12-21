90b2d029158921f57875ed9f6a226aac03f69477
Lelton Borges <leltondev@gmail.com>
2021-12-21 14:01:03 -0300

    add element in HTML

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const pacientes = [];
+// pacientes.push({name: 'Maria', count: 1, type: 'normal'});
+
 let count = 0;
 
 bnt_normal.addEventListener('click', function () {
@@ -9,28 +11,48 @@ bnt_normal.addEventListener('click', function () {
         count: ++count,
         type: 'normal'
     })
-    print()
 })
 
 bnt_prefe.addEventListener('click', function () {
     let name = pacient.value
     if (!name) return;
     let lastPref = pacientes.filter(p => p.type === 'preferencia').length
-    
+
     const obj = {
         name,
         count: ++count,
         type: 'preferencia'
     }
-    if(lastPref){
+    if (lastPref) {
         pacientes.splice(lastPref, 0, obj)
-    }else{
+    } else {
         pacientes.unshift(obj)
-        
+
     }
-    print()
 })
 
-const print = function () {
-    console.log(pacientes);
+const createElement = (type = 'li', id, content = '') => {
+    let element = document.createElement(type);
+    element.textContent = `${id+1}º - ${content}`;
+
+    return element;
 }
+
+const loadArray = () => {
+    const list = document.querySelector('section#lista');
+    if (pacientes.length) {
+        let ul = document.createElement('ul')
+        pacientes.forEach((v, k) => {
+            ul.appendChild(createElement('li', k, v.name))
+        })
+        list.appendChild(ul)
+    } else {
+        let p = document.createElement('p');
+        p.textContent = 'Sem pacientes'
+        list.appendChild(p)
+    }
+}
+
+window.addEventListener('load', function(){
+    loadArray()
+})
